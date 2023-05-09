5fbc36c960ead8b9adaf82c28ef83c2319ea4302
makabe0817 <131425399+makabe0817@users.noreply.github.com>
2023-05-09 12:36:22 +0900

    notu

--- Assets/Notu2.cs
+++ Assets/Notu2.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Notu2 : MonoBehaviour
 {
+    float speed;
+
+    private void Start()
+    {
+        speed = 10;
+    }
   
     void Update()
     {
-        transform.Translate(0, -10*Time.deltaTime, 0);
+        transform.Translate(0, -speed*Time.deltaTime, 0);
     }
 }
